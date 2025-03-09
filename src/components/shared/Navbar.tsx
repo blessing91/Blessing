026ed4861703d011cf5fb2db771e3c82412a514e
Tosin Amuda <tosinamuda@gmail.com>
2025-03-09 15:39:07 +0100

    feat(navbar): add blog

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -35,6 +35,7 @@ const Navbar = () => {
     { name: 'About', path: '/about' },
     { name: 'Services', path: '/services' },
     { name: 'Portfolio', path: '/portfolio' },
+    { name: 'Blog', path: '/blog' },
     { name: 'Contact', path: '/contact' },
   ];
 
